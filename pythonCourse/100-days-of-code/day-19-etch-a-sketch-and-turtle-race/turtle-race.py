d363048700cbe1b43093e6c0ca1a691b8151bd76
Tuanmonn <63285402+tuanmonn@users.noreply.github.com>
2021-08-04 20:52:59 +0700

    Update the race speed & print the winning turtle

--- pythonCourse/100-days-of-code/day-19-etch-a-sketch-and-turtle-race/turtle-race.py
+++ pythonCourse/100-days-of-code/day-19-etch-a-sketch-and-turtle-race/turtle-race.py
@@ -1,32 +1,47 @@
 # Practice file
 
 from turtle import Turtle, Screen
+import random
 
-
+is_race_on = False
+all_turtles = []
 screen = Screen()
 
 # setup the screen size
 screen.setup(width=500, height=400)
 
 # create a popup for user input
-# user_input = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color:")
+user_input = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color:")
 colors = ["red","orange","yellow","green","blue","purple"]
 
 x_base = -230
 y_base = -100
 
 def create_turtle(color):
-    tim = Turtle(shape="turtle")
-    tim.penup()
-    tim.color(color)
-    tim.goto(x= x_base,y=y_base)
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.penup()
+    new_turtle.color(color)
+    new_turtle.goto(x= x_base,y=y_base)
+    all_turtles.append(new_turtle)
 
 for i in colors:
     create_turtle(i)
     y_base += 50
 
+if user_input:
+    is_race_on = True
 
-
+while is_race_on:
+    for turtle in all_turtles:
+        if turtle.xcor() > 250:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_input:
+                print(f"You've won! The {winning_color} turtle is the winner!")
+            else:
+                print(f"You've lost!")
+        rand_distance = random.randint(0,10)
+        turtle.forward(rand_distance)
 
 
 
