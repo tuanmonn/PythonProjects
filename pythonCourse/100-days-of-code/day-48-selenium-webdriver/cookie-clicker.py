8ae8619b3399f1fc70ab0773233d61ea8007b76d
Tuanmonn <63285402+tuanmonn@users.noreply.github.com>
2021-09-12 16:05:17 +0700

    added comments for my code

--- pythonCourse/100-days-of-code/day-48-selenium-webdriver/cookie-clicker.py
+++ pythonCourse/100-days-of-code/day-48-selenium-webdriver/cookie-clicker.py
@@ -17,23 +17,23 @@ cookie = driver.find_element_by_id("cookie")
 part_of_name = "buy"
 all_ids = driver.find_elements_by_css_selector('[id*="%s"]' % part_of_name)
 
-# Create the list
+# Create the lists
 item_list = []
 price_list = []
 
 # Get the items' names and their prices:
 for n in range(0,len(all_ids)-1):
-    result = all_ids[n].text
-    get_list = result.split("-")
-    get_item_name = get_list[0].strip()
-    price_and_text_list = get_list[1].split("\n")
-    get_price = price_and_text_list[0].strip()
-    get_price_int = int(get_price.replace(",", ""))
+    result = all_ids[n].text                                # get the text of each id
+    get_list = result.split("-")                            # split the result for the name and the price
+    get_item_name = get_list[0].strip()                     # get the item name
+    price_and_text_list = get_list[1].split("\n")           # the price is stuck with the description so I split it to a list
+    get_price = price_and_text_list[0].strip()              # get the price and strip the unnecessary space
+    get_price_int = int(get_price.replace(",", ""))         # convert to integer (important)
     price_list.append(get_price_int)
     item_list.append(get_item_name)
 
 
-# Function to click buy
+# Function to click buy on the most expensive items
 def buyItem(money):
     for n in range(len(price_list)-1,0,-1):
         if money > price_list[n]:
@@ -44,15 +44,19 @@ def buyItem(money):
 game = True
 while game:
     cookie.click()
-
     if time.time() > timeout:
         num_cookie = driver.find_element_by_id("money")
+
+        # get and convert the num of cookies
         asset = num_cookie.text
         if "," in asset:
             asset = asset.replace(",", "")
         asset_num = int(asset)
+
+        # buy the item
         buyItem(asset_num)
 
+        # increase the time 5 more seconds
         timeout = time.time() + 5
 
     # stop after 5 mins
@@ -61,6 +65,5 @@ while game:
 
 cps = driver.find_element_by_id("cps")
 print(cps.text)
-
 
 driver.quit()
