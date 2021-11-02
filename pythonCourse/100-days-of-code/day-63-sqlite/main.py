b40aad8ac875d69ea38e267ee5d49c83d7f2899f
Tuanmonn <63285402+tuanmonn@users.noreply.github.com>
2021-11-02 23:49:11 +0700

    day-63-completed

--- pythonCourse/100-days-of-code/day-63-sqlite/main.py
+++ pythonCourse/100-days-of-code/day-63-sqlite/main.py
@@ -4,16 +4,19 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+# create db using SQLite
 # db = sqlite3.connect("books-collection.db")
 # cursor = db.cursor()
 # cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
 # db.commit()
 
+
+# create db
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///new-books-collection.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-
+# create table
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(250), unique=True, nullable=False)
@@ -24,34 +27,59 @@ class Book(db.Model):
         return '<Book %r>' % self.title
 
 db.create_all()
-new_book = Book(id=1, title="Harry Potter", author="J. K. Rowling", rating=9.3)
-db.session.add(new_book)
-db.session.commit()
 
-all_books = []
+# new_book = Book(id=1, title="Harry Potter", author="J. K. Rowling", rating=9.3)
+# db.session.add(new_book)
+# db.session.commit()
 
 
-@app.route('/')
+## -- old ways to keep a dictionary of books --
+# all_books = []
+# dict = {}
+# dict["book"] = request.form["book_name"]
+# dict["author"] = request.form["author"]
+# dict["rating"] = request.form["rating"]
+# all_books.append(dict)
+# print(all_books)
+
+
+@app.route('/', methods=["GET","POST"])
 def home():
+    if request.method == "POST":
+        # add books to database
+        new_book = Book(title=request.form["book_name"], author=request.form["author"], rating=request.form["rating"])
+        db.session.add(new_book)
+        db.session.commit()
+
+    all_books = db.session.query(Book).all()
+
     if len(all_books) == 0:
         msg = "There's no book in your lib"
         return render_template("index.html", msg=msg)
     else:
         return render_template("index.html", all_books=all_books)
 
-
-@app.route("/add", methods=["GET", "POST"])
-def add():
+@app.route('/update_rating', methods=["GET","POST"])
+def update_rating():
     if request.method == "POST":
-        dict = {}
-        dict["book"] = request.form["book_name"]
-        dict["author"] = request.form["author"]
-        dict["rating"] = request.form["rating"]
-        all_books.append(dict)
-        print(all_books)
+        # update the book rating
+        book_id = request.form["id"]
+        book_to_update = Book.query.get(book_id)
+        book_to_update.rating = request.form["new_rating"]
+        db.session.commit()
+        return redirect(url_for('home'))
+    all_books = db.session.query(Book).all()
+    return render_template("index.html", all_books=all_books)
 
+
+@app.route("/add")
+def add():
     return render_template("add.html")
 
+@app.route("/edit?id=<id>")
+def edit(id):
+    book_to_display = Book.query.filter_by(id=id).first()
+    return render_template("edit.html", book_name= book_to_display.title, book_rating= book_to_display.rating, book_id= book_to_display.id)
 
 if __name__ == "__main__":
     app.run(debug=True)
