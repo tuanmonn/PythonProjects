f8a2b7beff4c37fb7324983c252f5f01b22e4669
Tuanmonn <63285402+tuanmonn@users.noreply.github.com>
2021-11-17 21:30:25 +0700

    Complete project: Publish API doc

--- pythonCourse/100-days-of-code/day-66-build-API with-RESTful-routing/main.py
+++ pythonCourse/100-days-of-code/day-66-build-API with-RESTful-routing/main.py
@@ -10,19 +10,20 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+secret_api_key = "123456"
 
 ##Cafe TABLE Configuration
 class Cafe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(250), unique=True, nullable=False)
-    map_url = db.Column(db.String(500), nullable=False)
-    img_url = db.Column(db.String(500), nullable=False)
-    location = db.Column(db.String(250), nullable=False)
-    seats = db.Column(db.String(250), nullable=False)
-    has_toilet = db.Column(db.Boolean, nullable=False)
-    has_wifi = db.Column(db.Boolean, nullable=False)
-    has_sockets = db.Column(db.Boolean, nullable=False)
-    can_take_calls = db.Column(db.Boolean, nullable=False)
+    map_url = db.Column(db.String(500), nullable=True)
+    img_url = db.Column(db.String(500), nullable=True)
+    location = db.Column(db.String(250), nullable=True)
+    seats = db.Column(db.String(250), nullable=True)
+    has_toilet = db.Column(db.Boolean, nullable=True)
+    has_wifi = db.Column(db.Boolean, nullable=True)
+    has_sockets = db.Column(db.Boolean, nullable=True)
+    can_take_calls = db.Column(db.Boolean, nullable=True)
     coffee_price = db.Column(db.String(250), nullable=True)
 
     def to_dict(self):
@@ -78,6 +79,49 @@ def search_cafe():
         return jsonify(error={"Error": "Not found"})
 
 
+@app.route("/add", methods=["POST"])
+def post_new_cafe():
+    new_cafe = Cafe(
+        name=request.form.get("name"),
+        map_url=request.form.get("map_url"),
+        img_url=request.form.get("img_url"),
+        location=request.form.get("loc"),
+        has_sockets=bool(request.form.get("sockets")),
+        has_toilet=bool(request.form.get("toilet")),
+        has_wifi=bool(request.form.get("wifi")),
+        can_take_calls=bool(request.form.get("calls")),
+        seats=request.form.get("seats"),
+        coffee_price=request.form.get("coffee_price"),
+    )
+    db.session.add(new_cafe)
+    db.session.commit()
+    return jsonify(response={"success": "Successfully added the new cafe."})
+
+
+@app.route("/update-price/<int:id>", methods=["PATCH"])
+def update_price(id):
+    new_price = request.args.get("new_price")
+    cafe = Cafe.query.filter_by(id=id).first()
+    if cafe:
+        cafe.coffee_price = new_price
+        db.session.commit()
+        return jsonify({"success": "Successfully updated the price"}),200
+    else:
+        return jsonify({"Error": "We couldn't find the cafe with that id"}),404
+
+@app.route("/report-closed/<int:id>", methods=["DELETE"])
+def delete_cafe(id):
+    api_key = request.args.get("api_key")
+    cafe = Cafe.query.filter_by(id=id).first()
+    if api_key == secret_api_key:
+        if cafe:
+            Cafe.query.filter_by(id=id).delete()
+            db.session.commit()
+            return jsonify({"Success": "We have deleted that cafe!"})
+        else:
+            return jsonify({"Error": "We couldn't find the cafe with that id"}), 404
+    else:
+        return jsonify({"Error": "You don't have the permission to delete cafe"})
 
 
 ## HTTP GET - Read Record
